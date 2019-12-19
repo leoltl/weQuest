821de4e42e48181841b474beabe715a79b40004b
Kevin Laburte <kevin.laburte@gmail.com>
2019-12-19 17:09:23 -0500

    Fix server setup

--- frontEnd/gulpfile.js
+++ frontEnd/gulpfile.js
@@ -8,5 +8,11 @@ const copyBuildtoBackend = function(cb) {
 
 };
 
+function watchFiles(cb) {
+  console.log('Watching files. Hit ^C to stop.');
+  watch(`${projectSrc}**/*.*`, {delay: 200}, copyBuildtoBackend);
+}
+
 exports.default = copyBuildtoBackend;
 exports.build = copyBuildtoBackend;
+exports.watch = watchFiles;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,26 @@
+import path = require('path');
+
 // load .env data into process.env
 import { config } from 'dotenv';
 config();
 
 // Web server config
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || 'development';
 
-import * as express from 'express';
-import * as morgan from 'morgan';
+import express = require('express');
+import morgan = require('morgan');
 
 // instantiate express
 const app = express();
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('Hey');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hey');
+// });
 
-app.use('/test', express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // tslint:disable-next-line
 app.listen(PORT, () => console.log(`weQuest app listening on port ${PORT}`));
