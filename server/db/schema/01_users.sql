b4e081e1379cf55c1c92de0d05c6c575e0237c85
Leo Lee <tl.leolee@gmail.com>
2019-12-20 17:10:30 +0000

    added seed file

--- server/db/schema/01_users.sql
+++ server/db/schema/01_users.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  username VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
   password_digest VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  postal_code VARCHAR(255),
+  email VARCHAR(355) UNIQUE NOT NULL,
+  postal_code VARCHAR(10),
   latitude REAL,
   longtitude REAL
-);
+)
