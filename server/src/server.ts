5e105e1addc50fc70928c14db169157726995d76
Leo <tl.leolee@gmail.com>
2019-12-30 10:01:45 -0500

    revert back to previous version before merge

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,10 @@
-import path = require('path');
+import App from './app';
 
-// load .env data into process.env
-import { config } from 'dotenv';
-config();
-
-// Web server config
-const PORT = process.env.PORT || 8080;
-const ENV = process.env.ENV || 'development';
-
-import express = require('express');
+import * as bodyParser from 'body-parser';
 import morgan = require('morgan');
+import UserController from './routes/user.router';
 
+<<<<<<< HEAD
 // instantiate express
 const app = express();
 
@@ -24,3 +18,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // tslint:disable-next-line
 app.listen(PORT, () => console.log(`weQuest app listening on port ${PORT}`));
+=======
+import { config } from 'dotenv';
+config();
+
+const app = new App({
+  port: process.env.PORT || '8080',
+  controllers: [new UserController()],
+  middleWares: [
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: true }),
+    morgan('dev'),
+  ],
+});
+
+app.listen();
+>>>>>>> dd77837fbf2311dfb2a9bb27fb544c8cc89db5bc
