3de8fcc4071591f6903cf77779164492809974ba
Leo <tl.leolee@gmail.com>
2020-01-01 13:01:58 -0500

    basic styling to request card

--- frontEnd/src/components/RequestList/RequestListItem.jsx
+++ frontEnd/src/components/RequestList/RequestListItem.jsx
@@ -3,26 +3,50 @@ import {
   IonCard,
   IonCardHeader,
   IonCardSubtitle,
-  IonCardTitle,
-  IonCardContent
+  IonCardContent,
+  IonButton
 } from '@ionic/react';
 
 const RequestListItem = props => {
+  const isLoggedIn = true;
   return (
     <IonCard onClick={props.selectCard}>
       <IonCardHeader>
         <div className="request-card--header">
-          <IonCardTitle>Request Title</IonCardTitle>
-          <IonCardSubtitle>$30.00</IonCardSubtitle>
+          <IonCardSubtitle
+            style={{
+              fontSize: '1.15rem',
+              padding: 0
+            }}
+          >
+            Request Title
+          </IonCardSubtitle>
+          <IonCardContent
+            style={{
+              fontSize: '1.3rem',
+              padding: 0
+            }}
+          >
+            $30.00
+          </IonCardContent>
         </div>
-        <IonCardContent>
-          <div className="request-card--user">
-            <img alt="user avatar" src="https://i.pravatar.cc/50"></img>4/5
-          </div>
+        <IonCardContent className="request-card--user">
+          <img alt="user avatar" src="https://i.pravatar.cc/50"></img>
+          <span class="request-card--user-rating"></span>4/5
         </IonCardContent>
       </IonCardHeader>
       {props.isSelected ? (
-        <IonCardContent>{props.requestDetails.description}</IonCardContent>
+        <>
+          <IonCardContent>{props.requestDetails.description}</IonCardContent>
+          <IonButton
+            className="ion-margin"
+            disabled={isLoggedIn ? false : true}
+            expand="block"
+            type="submit"
+          >
+            Bid Now
+          </IonButton>
+        </>
       ) : (
         ''
       )}
