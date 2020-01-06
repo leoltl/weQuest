291b2ab67189d7d496ca81f5940f1a1a67b0b979
checksonhk <jackson.to@gmail.com>
2020-01-06 15:21:27 -0500

    fix merge conflicts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,7 @@
 // tslint:disable: import-name
 
 import App from './app';
-import cors = require('cors');
-import UserController from './routes/user.router';
+import UserController from './routes/users';
 import RequestController from './routes/request-router';
 import RequestControllerProtected from './routes/request-router-protected';
 import ItemRouter from './routes/items';
@@ -30,27 +29,22 @@ const storage = new Storage(storageParams);
 const app = new App({
   port: parseInt(process.env.PORT || '8080', 10),
   controllers: [
-    new UserController(),
     new RequestController(),
     new RequestControllerProtected(),
     new ItemRouter(db, storage),
+    new UserController(db),
   ],
   middlewares: [
     morgan('dev'),
     bodyParser.json({ limit: '10mb' }),
     bodyParser.urlencoded({ limit: '10mb', extended: true }),
     cookieSession({
-      name:'session',
+      name: 'session',
       keys: ['Coolstuffgoesonhere'],
       maxAge: 365 * 24 * 60 * 60 * 1000 /* 1 year */,
     }),
-    cors(),
   ],
 });
-
-import UserRouter from './routes/users';
-const userrouter = new UserRouter(db);
-app.use(userrouter.path, userrouter.router);
 
 // dummy login for dev
 app.app.get('/api/login/:id', async (req, res) => {
