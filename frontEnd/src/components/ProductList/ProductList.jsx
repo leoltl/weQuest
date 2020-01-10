e47bcf3de5a87dafa0749b248babab3514c1d119
checksonhk <jackson.to@gmail.com>
2020-01-10 15:57:32 -0500

    add product list styling

--- frontEnd/src/components/ProductList/ProductList.jsx
+++ frontEnd/src/components/ProductList/ProductList.jsx
@@ -3,34 +3,37 @@ import { IonSegment } from '@ionic/react';
 import { add } from 'ionicons/icons';
 import ProductFormModal from '../../pages/ProductFormModal';
 import ProductListItem from './ProductListItem';
+import './ProductList.scss';
 
 export default function ProductList({ products, product, setProduct, addProduct }) {
-
   console.log('rendering product list');
 
   const [showProductForm, setShowProductForm] = useState(false);
 
-  const selectProduct = useCallback((e) => {
-    const value = e.currentTarget.value;
-    
-    if (value === 'add') {
-      e.currentTarget.value = null;
-      setProduct(null);
-      setShowProductForm(true);
-    } else {
-      setProduct(value);
-    }
+  const selectProduct = useCallback(
+    e => {
+      const value = e.currentTarget.value;
 
-  },[setProduct]);
+      if (value === 'add') {
+        e.currentTarget.value = null;
+        setProduct(null);
+        setShowProductForm(true);
+      } else {
+        setProduct(value);
+      }
+    },
+    [setProduct],
+  );
 
   return (
     <>
-      <ProductFormModal {...{ showModal: showProductForm, setShowModal: setShowProductForm, onSuccess: addProduct} } />
-      <IonSegment scrollable onIonChange={selectProduct}>
-        <ProductListItem {...{ key:'add', id: 'add', name: 'Add Item', pictureUrl: add, isIcon: true }} />
-        {products.map((p) => <ProductListItem {...{ key: p.id, ...p, isSelected: product === p.id }} />)}
+      <ProductFormModal {...{ showModal: showProductForm, setShowModal: setShowProductForm, onSuccess: addProduct }} />
+      <IonSegment className='product-list' scrollable onIonChange={selectProduct}>
+        <ProductListItem {...{ key: 'add', id: 'add', name: 'Add Item', pictureUrl: add, isIcon: true }} />
+        {products.map(p => (
+          <ProductListItem className='product-list-item' {...{ key: p.id, ...p, isSelected: product === p.id }} />
+        ))}
       </IonSegment>
     </>
   );
-
- }
+}
--- frontEnd/src/pages/BidFormModal.jsx
+++ frontEnd/src/pages/BidFormModal.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import Modal from '../components/Modal';
 import ErrorAlert from '../components/ErrorAlert';
-import ProductList from "../components/ProductList/ProductList";
+import ProductList from '../components/ProductList/ProductList';
 
 // dummy data
 const dummyProducts = [
@@ -12,32 +12,37 @@ const dummyProducts = [
     id: 1,
     name: 'Barbecue',
     description: 'product description',
-    pictureUrl: 'https://images.unsplash.com/photo-1500840922267-8ff91fbf85aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80'
+    pictureUrl:
+      'https://images.unsplash.com/photo-1500840922267-8ff91fbf85aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
   },
   {
     id: 2,
     name: 'Drill',
     description: 'product description',
-    pictureUrl: 'https://images.unsplash.com/photo-1504148455328-c376907d081c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1686&q=80'
+    pictureUrl:
+      'https://images.unsplash.com/photo-1504148455328-c376907d081c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1686&q=80',
   },
   {
     id: 3,
     name: 'Clown',
     description: 'product description',
-    pictureUrl: 'https://images.unsplash.com/photo-1502488207239-dcf4114041cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+    pictureUrl:
+      'https://images.unsplash.com/photo-1502488207239-dcf4114041cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
   },
   {
     id: 4,
     name: 'Keyboard',
     description: 'product description',
-    pictureUrl: 'https://images.unsplash.com/photo-1545112719-ce81d7de0b71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1622&q=80'
+    pictureUrl:
+      'https://images.unsplash.com/photo-1545112719-ce81d7de0b71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1622&q=80',
   },
   {
     id: 5,
     name: 'Camera',
     description: 'product description',
-    pictureUrl: 'https://images.unsplash.com/photo-1519638831568-d9897f54ed69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
-  }
+    pictureUrl:
+      'https://images.unsplash.com/photo-1519638831568-d9897f54ed69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  },
 ];
 
 const bidInitialState = {
@@ -54,43 +59,42 @@ const bidActions = {
   ADD_PRODUCT: 2,
   SET_PRICE: 3,
   SET_NOTES: 4,
-  RESET: 5
+  RESET: 5,
 };
 
 function bidReducer(state, { type, payload }) {
-
-  switch(type) {
+  switch (type) {
     case bidActions.PRODUCT_DATA:
       return {
         ...state,
         products: payload.products,
       };
-    
+
     case bidActions.SET_PRODUCT:
       return {
         ...state,
-        product: payload.product
+        product: payload.product,
       };
-        
+
     case bidActions.ADD_PRODUCT:
       return {
         ...state,
         products: state.products.concat(payload.product),
-        product: payload.product.id
+        product: payload.product.id,
       };
-          
+
     case bidActions.SET_PRICE:
       return {
         ...state,
-        price: payload.price
+        price: payload.price,
       };
 
     case bidActions.SET_NOTES:
       return {
         ...state,
-        notes: payload.notes
+        notes: payload.notes,
       };
-    
+
     case bidActions.RESET:
       return {
         ...state,
@@ -101,13 +105,10 @@ function bidReducer(state, { type, payload }) {
 
     default:
       throw Error(`Tried to reduce with unsupported action of type ${type}.`);
-
   }
-
 }
 
 export default function BidFormModal({ showModal, setShowModal, request, updateRequestById }) {
-
   console.log('rendering bid form');
 
   const [showSpinner, setShowSpinner] = useState(false);
@@ -116,50 +117,53 @@ export default function BidFormModal({ showModal, setShowModal, request, updateR
   // state reducer
   const [bidState, bidDispatch] = useReducer(bidReducer, bidInitialState);
 
-  const setProduct = useCallback((product) => {
+  const setProduct = useCallback(product => {
     bidDispatch({ type: bidActions.SET_PRODUCT, payload: { product: parseInt(product) || null } });
   }, []);
 
-  const addProduct = useCallback((product) => {
+  const addProduct = useCallback(product => {
     bidDispatch({ type: bidActions.ADD_PRODUCT, payload: { product } });
   }, []);
 
-  const setPrice = useCallback((price) => {
-    bidDispatch({ type: bidActions.SET_PRICE, payload: { price: Math.min(parseInt(price), request.priceCent * 100) } });
-  }, [request]);
+  const setPrice = useCallback(
+    price => {
+      bidDispatch({ type: bidActions.SET_PRICE, payload: { price: Math.min(parseInt(price), request.priceCent * 100) } });
+    },
+    [request],
+  );
 
-  const setNotes = useCallback((notes) => {
+  const setNotes = useCallback(notes => {
     bidDispatch({ type: bidActions.SET_NOTES, payload: { notes } });
   }, []);
 
-  const submitBid = (e) => {
+  const submitBid = e => {
     e.preventDefault();
     e.stopPropagation();
     console.log('submitting bid');
 
     if (!bidState.product) return setErrorMessage('You must select an item to bid with!');
     if (bidState.price >= request.priceCent) return setErrorMessage('Your price is too high relative to the latest bids!');
-    
+
     setShowSpinner('Saving...');
 
     const bid = {
       requestId: request.id,
       itemId: bidState.product,
       priceCent: bidState.price,
-      notes: bidState.notes
+      notes: bidState.notes,
     };
     // TODO: replace resolve with axios call
     // new Promise((resolve) => {
     //   setTimeout(() => resolve({ data: bid }), 3000);
     // })
-    axios.post('/api/bids', bid)
-    .then(({ data: { requestId, priceCent } }) => {
-      setShowModal(false);
-      updateRequestById(requestId, { priceCent });
-    })
-    .catch((err) => setErrorMessage(err.message))
-    .finally(() => setShowSpinner(false));
-
+    axios
+      .post('/api/bids', bid)
+      .then(({ data: { requestId, priceCent } }) => {
+        setShowModal(false);
+        updateRequestById(requestId, { priceCent });
+      })
+      .catch(err => setErrorMessage(err.message))
+      .finally(() => setShowSpinner(false));
   };
 
   // reset form if request id or priceCent change
@@ -169,48 +173,63 @@ export default function BidFormModal({ showModal, setShowModal, request, updateR
 
   // load product data
   useEffect(() => {
-
     console.log('load initial bid form data');
     setShowSpinner(true);
-    
+
     // TODO: replace resolve with axios call
     // new Promise((resolve) => {
     //   setTimeout(() => resolve({ data: dummyProducts }), 3000);
     // })
-    axios.get('/api/items')
-    .then(({ data: products }) => {
-      bidDispatch({ type: bidActions.PRODUCT_DATA, payload: { products } });
-    })
-    .catch((err) => setErrorMessage(err.message))
-    .finally(() => setShowSpinner(false));
-
+    axios
+      .get('/api/items')
+      .then(({ data: products }) => {
+        bidDispatch({ type: bidActions.PRODUCT_DATA, payload: { products } });
+      })
+      .catch(err => setErrorMessage(err.message))
+      .finally(() => setShowSpinner(false));
   }, []);
 
   return (
-    <Modal {...{ showModal, setShowModal, showSpinner, title: `Bid on ${ request.title}` }}>
+    <Modal {...{ showModal, setShowModal, showSpinner, title: `Bid on ${request.title}` }}>
       {errorMessage && <ErrorAlert {...{ message: errorMessage, clear: () => setErrorMessage('') }} />}
       <form onSubmit={submitBid}>
-        {/* <h3>Pick a Product</h3> */}
-        <IonList>
+        <IonList className='bid-form-modal__list-container'>
           {/* Label and product list should be under one and the same item component. Need to fix formatting */}
-          <IonItem>
-            <IonLabel position='stacked' style={{ marginBottom: 20 }}>Pick a Product</IonLabel>
+          <IonItem lines='none'>
+            <IonLabel className='bid-form-modal__item-title'>Pick a Product</IonLabel>
           </IonItem>
-          <IonItem>
-            <ProductList { ...{ products: bidState.products, product: bidState.product, setProduct, addProduct } } />
-          </IonItem>
-          {/* <h3>Name Your Price</h3> */}
+          <ProductList {...{ products: bidState.products, product: bidState.product, setProduct, addProduct }} />
           <IonItem>
             <IonLabel position='floating'>Name Your Price</IonLabel>
-            <IonInput type='number' name='price' max={Math.max((request.priceCent - 100) / 100, 0)} min={0} value={bidState.price / 100} step={0.5} inputmode='decimal' onIonChange={(e) => setPrice(e.currentTarget.value * 100)} debounce={100} required></IonInput>
+            <IonInput
+              type='number'
+              name='price'
+              max={Math.max((request.priceCent - 100) / 100, 0)}
+              min={0}
+              value={bidState.price / 100}
+              step={0.5}
+              inputmode='decimal'
+              onIonChange={e => setPrice(e.currentTarget.value * 100)}
+              debounce={100}
+              required
+            ></IonInput>
           </IonItem>
           {/* <h3>Notes</h3> */}
           <IonItem>
-            <IonLabel position='floating'>Notes</IonLabel>
-            <IonTextarea name='notes' value={bidState.notes} rows={4} spellcheck onIonChange={(e) => setNotes(e.currentTarget.value)} debounce={100}></IonTextarea>
+            <IonLabel position='stacked'>Notes</IonLabel>
+            <IonTextarea
+              name='notes'
+              value={bidState.notes}
+              spellcheck
+              onIonChange={e => setNotes(e.currentTarget.value)}
+              debounce={100}
+              autoGrow
+            ></IonTextarea>
           </IonItem>
         </IonList>
-        <IonButton type='submit'>Bid</IonButton>
+        <IonButton expand={'block'} type='submit'>
+          Bid Now
+        </IonButton>
       </form>
     </Modal>
   );
